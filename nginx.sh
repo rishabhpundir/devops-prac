2b8f69a665b39acbb8565c5350f3e6acba15dfe3
rishabhpundir <th3rishabh@gmail.com>
2024-03-28 17:25:33 +0530

    fixed nginx shell 2

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 710 /var/lib/jenkins/workspace/DjangoTest/
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/
 sudo nginx -t
 
 sudo systemctl start nginx
