a4797fdc88de6a329ae72c6c9bf2da2df54e6b91
rishabhpundir <th3rishabh@gmail.com>
2024-03-28 16:55:26 +0530

    updated echo statments

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d "env" ]; then
-    echo "Python venv exists!"
+    echo "********Python venv exists!********"
 else
     python3 -m venv env
 fi
@@ -12,11 +12,11 @@ source env/bin/activate
 pip3 install -r requirements.txt
 
 if [ -d "logs" ]; then
-    echo "Log Folders exist!"
+    echo "********Log Folders exist!********"
 else
     mkdir logs
     touch logs/error.log logs/access.log
 fi
 
 sudo chmod -R 777 logs
-echo "venv setup finishes"
+echo "********venv setup finishes********"
--- gunicorn.sh
+++ gunicorn.sh
@@ -7,21 +7,21 @@ cd /var/lib/jenkins/workspace/DjangoTest
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo "Migrations done!"
+echo "********Migrations done!********"
 
 cd /var/lib/jenkins/workspace/DjangoTest
 
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
 
-echo "$USER"
-echo "$PWD"
+echo "********$USER********"
+echo "********$PWD********"
 
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 
-echo "Gunicorn has been started!"
+echo "********Gunicorn has been started!********"
 
 sudo systemctl status gunicorn
 sudo systemctl restart gunicorn
