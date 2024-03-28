7390d65557367a70b59f49a11fd9b2a06a615f12
rishabhpundir <th3rishabh@gmail.com>
2024-03-28 16:26:45 +0530

    added gunicorn files

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-source venv/bin/activate
-cd hello
-gunicorn hello.wsgi:application --bind 0.0.0.0:8000
+source env/bin/activate
+
+cd /var/lib/jenkins/workspace/DjangoTest/hello
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Migrations done!"
+
+cd /var/lib/jenkins/workspace/DjangoTest
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been started!"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
