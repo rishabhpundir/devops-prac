279879c87f4c9527dc0265b54094bd2115fbe67d
rishabhpundir <th3rishabh@gmail.com>
2024-03-28 16:38:49 +0530

    fix

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-cd /var/lib/jenkins/workspace/DjangoTest
 source env/bin/activate
+cd /var/lib/jenkins/workspace/DjangoTest
 
 python3 manage.py makemigrations
 python3 manage.py migrate
